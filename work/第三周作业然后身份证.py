4677e71907af63fab4ce30d4e141eae07ebf0ba3
yuy zhou <2072707317@qq.com>
2026-09-14 13:01:18 +0800

    作业

--- work/第三周作业然后身份证.py
+++ work/第三周作业然后身份证.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-sfz = "441322200312206010"
+sfz = input("请输入身份证号")
+sex=["男","女"]
 year = datetime.now().year
-print(year)
+print(f"你经年：{year-int(sfz[6:10])}岁了 是{sex[1 - int(sfz[16]) % 2]}")
 print(sfz[16])  #奇数男   偶数女
